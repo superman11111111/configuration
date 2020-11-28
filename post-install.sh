26532c25f2e1296de5fe1ee612d21c12c0ee0109
mkypr <superman222222223333333@gmail.com>
2020-11-28 15:14:36 +0100

    fix

--- post-install.sh
+++ post-install.sh
@@ -5,11 +5,12 @@ for i in Documents/ Music/ Pictures/ Public/ Templates/ Videos/
 do 
 	sudo rm -rf ~/$i
 done 
-mv ~/Downloads/ ~/downloads/ 2>/dev/null && 
-mv ~/Desktop/ ~/desktop/ 2>/dev/null && 
-sudo cp -rf user-dirs.dirs ~/.config/user-dirs.dirs &&
-cp -f .tmux.conf ~/.tmux.conf &&
-cat .bashrc >> ~/.bashrc &&
-cp .screenrc ~/.screenrc &&
+#mv ~/Downloads/ ~/downloads/ 2>/dev/null && 
+#mv ~/Desktop/ ~/desktop/ 2>/dev/null && 
+#sudo cp -rf user-dirs.dirs ~/.config/user-dirs.dirs &&
+#cp -f .tmux.conf ~/.tmux.conf &&
+#cat .bashrc >> ~/.bashrc &&
+#cp .screenrc ~/.screenrc &&
 cp .vimrc ~/.vimrc
+echo $?
 sudo apt autoremove
