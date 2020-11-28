103f45652db6b662d3c12d8c0b19d1a8cce98514
mkypr <superman222222223333333@gmail.com>
2020-11-28 15:09:10 +0100

    vimrc

---
+++ .vimrc
@@ -0,0 +1,7 @@
+syntax on
+set expandtab
+set ai
+set number
+set hlsearch
+set ruler
+highlight Comment ctermfg=green
--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 sudo apt update -y 2> /dev/null && 
 sudo apt upgrade -y 2>/dev/null && 
-sudo apt install build-essential tmux screen vim git net-tools python3-pip -y && 
+sudo apt install build-essential tmux screen vim git net-tools python3-pip htop -y && 
 for i in Documents/ Music/ Pictures/ Public/ Templates/ Videos/ 
 do 
 	sudo rm -rf ~/$i
@@ -11,4 +11,5 @@ sudo cp -rf user-dirs.dirs ~/.config/user-dirs.dirs &&
 cp -f .tmux.conf ~/.tmux.conf &&
 cat .bashrc >> ~/.bashrc &&
 cp .screenrc ~/.screenrc &&
+cp .vimrc ~/.vimrc
 sudo apt autoremove
